6ca6c4bd3604b2b20a4e12f69c4b1f02ec582dc9
Anas Karkoukli <karkoukli.anas@gmail.com>
2020-06-22 14:45:01 -0300

    show auth token to use

--- ak.companies.web/src/components/navigation/profile.js
+++ ak.companies.web/src/components/navigation/profile.js
@@ -6,6 +6,13 @@ import {
   MenuItem,
   ListItemIcon,
   ListItemText,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Tooltip,
+  Fab,
 } from "@material-ui/core";
 import AccountIcon from "@material-ui/icons/AccountCircleRounded";
 import LogoutIcon from "@material-ui/icons/ExitToAppRounded";
@@ -13,12 +20,35 @@ import EmailIcon from "@material-ui/icons/EmailRounded";
 import TokenIcon from "@material-ui/icons/VpnKeyRounded";
 import LoginIcon from "@material-ui/icons/LockOpenRounded";
 import DropDownIcon from "@material-ui/icons/ArrowDropDownRounded";
+import CopyIcon from "@material-ui/icons/FileCopyRounded";
+import CloseIcon from "@material-ui/icons/CloseRounded";
 
 export default () => {
   const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
   const [anchorEl, setAnchorEl] = useState(null);
   const isMenuOpen = Boolean(anchorEl);
   const handleMenuClose = () => setAnchorEl(null);
+  const [tokenDialogOpen, setTokenDialogOpen] = useState(false);
+  const [token, setToken] = useState();
+  const { getTokenSilently } = useAuth0();
+
+  const showToken = async () => {
+    setTokenDialogOpen(true);
+    const token = await getTokenSilently();
+    setToken(token);
+  };
+
+  const hideToken = () => setTokenDialogOpen(false);
+
+  const copyToken = async () => {
+    const result = await navigator.permissions.query({
+      name: "clipboard-write",
+    });
+
+    if (["granted", "prompt"].includes(result.state)) {
+      await navigator.clipboard.writeText(token);
+    }
+  };
 
   if (!isAuthenticated) {
     return (
@@ -31,7 +61,7 @@ export default () => {
       </Button>
     );
   }
-    
+
   return (
     <div>
       <Button
@@ -45,18 +75,19 @@ export default () => {
       >
         Profile
       </Button>
-      <Menu
-        anchorEl={anchorEl}
-        open={isMenuOpen}
-        onClose={handleMenuClose}
-      >
+      <Menu anchorEl={anchorEl} open={isMenuOpen} onClose={handleMenuClose}>
         <MenuItem disabled={true}>
           <ListItemIcon>
             <EmailIcon />
           </ListItemIcon>
           <ListItemText>{user.email}</ListItemText>
         </MenuItem>
-        <MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleMenuClose();
+            showToken();
+          }}
+        >
           <ListItemIcon>
             <TokenIcon />
           </ListItemIcon>
@@ -74,6 +105,32 @@ export default () => {
           <ListItemText>Log out</ListItemText>
         </MenuItem>
       </Menu>
+      <Dialog open={tokenDialogOpen} onClose={hideToken}>
+        <DialogTitle>This is your Auth Token</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            You can use it to test the API directly
+          </DialogContentText>
+          <DialogContentText
+            style={{ overflowWrap: "anywhere" }}
+            variant="caption"
+          >
+            {token}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Tooltip title="Copy to Clipboard">
+            <Fab size="small" color="primary" onClick={copyToken}>
+              <CopyIcon />
+            </Fab>
+          </Tooltip>
+          <Tooltip title="Close">
+            <Fab size="small" color="primary" onClick={hideToken}>
+              <CloseIcon />
+            </Fab>
+          </Tooltip>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
